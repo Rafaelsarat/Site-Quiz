c6d47087e5005e06d5cd1cbcd2bc69f32c06144c
unknown <aluno@aluno.com>
2019-04-11 10:08:31 -0300

    Alteração no inicio

--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -1,6 +1,5 @@
 <template>
-
-
+<div>
   <div class="text-xs-center">
      <v-toolbar>
       <v-menu bottom
@@ -10,6 +9,7 @@
       <template v-slot:activator="{ on }" >
         <v-toolbar-side-icon v-on="on" color="teal" outline></v-toolbar-side-icon>
       </template>
+      
       <v-list>
         <v-list-tile
           v-for="(item, index) in items"
@@ -19,8 +19,14 @@
         </v-list-tile>
       </v-list>
     </v-menu>
-    
     <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer/>
+    
+      <v-text-field label="Solo" single-line solo></v-text-field>
+         <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>  
+   
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn flat>Link One</v-btn>
@@ -28,7 +34,7 @@
       <v-btn flat>Link Three</v-btn>
     </v-toolbar-items>
   </v-toolbar>
-  
+  </div>  
   </div>
   
 </template>
@@ -47,39 +53,7 @@
 </script>
 <style>
   .rafa{
-	font-family: Arial, Helvetica, sans-serif;
-	font-size: 14px;
-  right: 45%;
-	color: #ffffff;
-	padding: 7px 20px;
-	background: -moz-linear-gradient(
-		top,
-		#f0f0f0 0%,
-		#b8b8b8 25%,
-		#6b6b6b 75%,
-		#424242);
-	background: -webkit-gradient(
-		linear, left top, left bottom,
-		from(#f0f0f0),
-		color-stop(0.25, #b8b8b8),
-		color-stop(0.75, #6b6b6b),
-		to(#424242));
-	-moz-border-radius: 17px;
-	-webkit-border-radius: 17px;
-	border-radius: 17px;
-	border: 3px solid #171717;
-	-moz-box-shadow:
-		0px 1px 1px rgba(000,000,000,0.5),
-		inset 0px 0px 10px rgba(087,087,087,0.7);
-	-webkit-box-shadow:
-		0px 1px 1px rgba(000,000,000,0.5),
-		inset 0px 0px 10px rgba(087,087,087,0.7);
-	box-shadow:
-		0px 1px 1px rgba(000,000,000,0.5),
-		inset 0px 0px 10px rgba(087,087,087,0.7);
-	text-shadow:
-		0px -1px 0px rgba(000,000,000,0.4),
-		0px 1px 2px rgba(255,255,255,0.3);
+    margin-top:33px;  
 }
 
 </style>
