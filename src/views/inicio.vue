e746738051bf64e77c04aa6459cfb6c694fdb157
unknown <aluno@aluno.com>
2019-04-29 16:15:11 -0300

    inicio certo

--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -1,59 +1,43 @@
 <template>
-    <div class="text-xs-center">
-    <v-menu offset-y>
-      <template v-slot:activator="{ on }">
-        <v-btn
-          color="primary"
-          dark
-          v-on="on"
-          class="rafa"
-        >
-          Menu
-        </v-btn>
+<div>
+  <div class="text-xs-center">
+     <v-toolbar>
+      <v-menu bottom
+        origin="center center"
+        transition="scale-transition"
+      >
+      <template v-slot:activator="{ on }" >
+        <v-toolbar-side-icon v-on="on" color="teal" outline></v-toolbar-side-icon>
+      </template>
+      
       <v-list>
         <v-list-tile
           v-for="(item, index) in items"
           :key="index"
-          @click=""
         >
           <v-list-tile-title>{{ item.title }}</v-list-tile-title>
         </v-list-tile>
       </v-list>
     </v-menu>
-  <div>
-  <v-carousel hide-delimiters>
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer/>
+    
+      <v-text-field label="Solo" single-line solo></v-text-field>
+         <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>  
+   
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+  </div>  
   </div>
-  </div>
+  
 </template>
-
-<script>
-  export default {
-    data () {
-      return {
-        items: [
-          {
-            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBWQm1yEbpGEG6YOWehV51naNaFZpxM1nLdcrXy_XQIVEYAtdr'
-          },
-          {
-            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQshtrMF4pZivmrPAdivUnuMEFcsuJ3lY6znRwJ2irScKFwSf5o'
-          },
-          {
-            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG_UHG0M_AnmazzLTfo3Zt5FTMz1462LTX80nCRjv8YAhGNmKzw'
-          },
-          {
-            src: 'http://noticias.universia.com.br/net/images/cultura/9/9-/9-t/9-testes-para-avaliar-sua-capacidade-mental.jpg'
-          }
-        ]
-      }
-    }
-  }
-</script>
 
 <script>
   export default {
@@ -68,5 +52,8 @@
   }
 </script>
 <style>
+  .rafa{
+    margin-top:33px;  
+}
 
 </style>
