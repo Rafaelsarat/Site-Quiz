1a5660f23350f40bc975e26c1f98c5f7fd33a471
unknown <aluno@aluno.com>
2019-04-29 17:26:07 -0300

    Carrosel e inicio corrigido

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,63 @@
 <template>
   <v-app>
+    <v-toolbar class="grey darken-3" dark>
+      <v-menu bottom
+        origin="center center"
+        transition="scale-transition"
+      >
+      <template v-slot:activator="{ on }" >
+        <v-toolbar-side-icon v-on="on" color="teal" outline></v-toolbar-side-icon>
+      </template>
+      <v-list>
+        <v-list-tile
+          v-for="(item, index) in items"
+          :key="index"
+          :to="item.rota"
+        >
+            <v-list-tile-action>
+		        	<v-icon>{{ item.icone }}</v-icon>
+		        </v-list-tile-action>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+    <v-toolbar-title>Quiz</v-toolbar-title>
+    <v-spacer/>
+    
+      <v-text-field label="Pesquisar" single-line solo></v-text-field>
+         <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>  
+   
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Suporte</v-btn>
+      <v-btn to="/inicio" flat>Inicio</v-btn>
+      <v-btn to="/novos teste" flat>Novos Teste</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
     <v-content>
       <router-view></router-view>
      </v-content> 
     </v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Ranking', icone:"search", rota:"/rank"},
+        { title: 'Meu Desempenho', icone:"assessment" , rota:"/desempenho"},
+        { title: 'Minhas Questões', icone:"playlist_add_check" , rota:"/questoes"},
+        { title: 'Meu Perfil', icone:"person", rota:"/perfil"},
+      ]
+    })
+  }
+</script>
+<style>
+  .rafa{
+    margin-top:33px;  
+}
+
+</style>
   
--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -1,59 +1,31 @@
 <template>
-<div>
-  <div class="text-xs-center">
-     <v-toolbar>
-      <v-menu bottom
-        origin="center center"
-        transition="scale-transition"
-      >
-      <template v-slot:activator="{ on }" >
-        <v-toolbar-side-icon v-on="on" color="teal" outline></v-toolbar-side-icon>
-      </template>
-      
-      <v-list>
-        <v-list-tile
-          v-for="(item, index) in items"
-          :key="index"
-        >
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-menu>
-    <v-toolbar-title>Title</v-toolbar-title>
-    <v-spacer/>
-    
-      <v-text-field label="Solo" single-line solo></v-text-field>
-         <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>  
-   
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
-  </div>  
-  </div>
-  
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
-
 <script>
   export default {
-    data: () => ({
-      items: [
-        { title: 'Ranking' },
-        { title: 'Meu Desempenho' },
-        { title: 'Minhas Questões' },
-        { title: 'Meu Perfil' }
-      ]
-    })
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
   }
 </script>
-<style>
-  .rafa{
-    margin-top:33px;  
-}
-
-</style>
